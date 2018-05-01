d712efbaee2fde1015a2e5d9ca8c022117f53da8
PhuongNguyen258 <n.phuong2508@gmail.com>
2018-05-01 22:05:56 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function() {
+    $('li').click(function(){
+        $('li').removeClass();
+        $(this).addClass('current');
+    });
+});*/
+function scrollWin() {
+    window.scrollBy(0, 500);
+}
